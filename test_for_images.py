26c5523c1398c99f0a1b535f9baf54bbec30ee35
FabioArdi <arditfab@students.zhaw.ch>
2023-06-10 18:13:19 +0200

    WIP

--- test_for_images.py
+++ test_for_images.py
@@ -1,51 +1,53 @@
 import requests
 import time
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import os
+
 
 # API endpoint
 url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
 
 # Request parameters
 params = {
-    "type": "Normal Monster"
+
 }
 
-# Number of images to download
-num_images = 3
+word_list = dict()
+subtype_list = dict()
+char_list = dict()
+card_number_listed = 0
 
 try:
-    # Send GET request to the API
     response = requests.get(url, params=params)
     data = response.json()
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Retrieve the list of cards
-        cards = data["data"]
-
-        # Iterate over the cards and download images
-        for i in range(num_images):
-            card = cards[i]
-            image_url = card["card_images"][0]["image_url_cropped"]
-            image_name = f"image_{i}.jpg"
-
-            # Download the image
-            response = requests.get(image_url)
-
-            # Check if the image download was successful
-            if response.status_code == 200:
-                # Save the image to disk
-                with open(image_name, "wb") as file:
-                    file.write(response.content)
-                print(f"Image {i + 1} downloaded and saved.")
-
-                # Wait for 2 seconds before downloading the next image
-                time.sleep(2)
-
-            else:
-                print(f"Failed to download image {i + 1} with status code:", response.status_code)
-
-    else:
-        print("Request failed with status code:", response.status_code)
-
+        cards = pd.DataFrame(data=data["data"])
+        
 except requests.exceptions.RequestException as e:
     print("An error occurred:", e)
+
+
+for index, row in cards.iterrows():
+    id = row['id']
+    cropped_image = row['card_images'][0]['image_url_cropped']
+
+    if os.path.exists(f"images/{id}.jpg"):
+        continue
+
+    # Download the image
+    response = requests.get(cropped_image)
+
+    # Check if the image download was successful
+    if response.status_code == 200:
+        # Save the image to disk
+        with open(f"images/{id}.jpg", "wb") as file:
+            file.write(response.content)
+
+        # Wait for 1 seconds before downloading the next image
+        time.sleep(1)
+
+    else:
+        print(f"Failed to download image {id} with status code:", response.status_code)
