963f735c46b7abbcdb2f7b6333c5fa1abeb17b02
FabioArdi <arditfab@students.zhaw.ch>
2023-06-10 20:09:54 +0200

    Cleanup

--- BLA.py
+++
@@ -1,7 +0,0 @@
-def count_characters(string):
-    return len(string)
-
-# Example usage
-input_string = input("Enter a string: ")
-character_count = count_characters(input_string)
-print("Number of characters:", character_count)
