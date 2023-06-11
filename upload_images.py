e4396028a547f27648622f60bc218cb4b9a79927
FabioArdi <arditfab@students.zhaw.ch>
2023-06-11 22:21:20 +0200

    Cleanup

--- upload_images.py
+++ upload_images.py
@@ -1,58 +1,37 @@
-from datasets import Dataset
-import requests
-import time
-import numpy as np
-import tensorflow as tf
 import pandas as pd
 import os
-from PIL import Image
-from io import BytesIO
-import base64
-from huggingface_hub import HfApi
+import shutil
+from datasets import load_dataset
 
-# API endpoint
-url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+df = pd.read_csv('deck.csv')
 
-# Request parameters
-params = {
+df_c = df[["id", "name", "frameType"]]
 
-}
+metadata = []
+for index, row in df_c.iterrows():
+    dir = f"train/{row['frameType']}"
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-word_list = dict()
-subtype_list = dict()
-char_list = dict()
-card_number_listed = 0
+    try:
+        sourceImage = f"images/{row['id']}.jpg"
+        targetImage = f"{dir}/{row['name']}.jpg"
 
-try:
-    response = requests.get(url, params=params)
-    data = response.json()
+        shutil.copy(sourceImage, targetImage) 
 
-    if response.status_code == 200:
-        cards = pd.DataFrame(data=data["data"])
-        
-except requests.exceptions.RequestException as e:
-    print("An error occurred:", e)
+        metadata.append({
+            'file_name': f"{row['frameType']}/{row['name']}.jpg",
+            'name': row['name'].replace("\"", ""),
+            'frameType': row['frameType']
+        })
+    except:
+        continue
 
-# Save the dataframe as csv
-cards.to_csv("deck.csv", index=False)
+df_m = pd.DataFrame.from_dict(metadata)
+df_m.to_csv('train/metadata.csv', index=False)
 
-def read_image(file_path):
-    with Image.open(file_path) as img:
-        return img.convert('RGB')
 
-df_c = cards[["id", "name"]].head(10)
-df_c['image'] = df_c.apply(lambda row: f"images/{row['id']}.jpg", axis=1)
-df_c['image_data'] = df_c['image'].apply(read_image)
 
-def image_to_base64(image):
-    buffered = BytesIO()
-    image.save(buffered, format="JPEG")
-    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+dataset = load_dataset("imagefolder", data_dir=".")
 
-df_c["image_data"] = df_c["image_data"].apply(image_to_base64)
-huggingface_dataset = Dataset.from_pandas(df_c)
-
-print(df_c.head(10))
-
-# Upload to Huggingface
-# huggingface_dataset.push_to_hub("FabioArdi/test", private=True)
+dataset.push_to_hub("FabioArdi/yugioh_images", private=True)
